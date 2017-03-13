e7e3236efc1145edfc23ed83182dd0536b570204
NearEast <teddy_nee@163.com>
2017-03-13 16:02:34 +0800

    自动获取项目名称：把createIPA.sh文件放到需要打包的工程根目录下即可；默认需要把DerivedData目录也设置在根目录下，或者可以直接修改脚本

--- createIPA.sh
+++ createIPA.sh
@@ -1,21 +1,25 @@
 
 version=$1
+projectName=`pwd | awk -F "/" '{print $NF}'`
+
+echo 'using version number '${version}
+echo 'using projectName '${projectName}
 
 dateToday=`date +%Y%m%d`
 NE_BUILDID=${dateToday}.`git rev-list HEAD | wc -l | awk '{print $1}'`
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${NE_BUILDID}" HandyPushStream/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${NE_BUILDID}" ${projectName}/Info.plist
 
 if [ -z $version ];then
 echo 'using old version number'
 else
 echo 'using new version:${version}'
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${version}" HandyPushStream/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${version}" ${projectName}/Info.plist
 fi
 
-xcodebuild -workspace HandyPushStream.xcworkspace -scheme HandyPushStream -configuration Release clean build ARCHS='armv7 arm64' -sdk iphoneos10.2 CODE_SIGN_IDENTITY="iPhone Distribution: Leshi Co., Ltd" PROVISIONING_PROFILE=CommonEntpInHouseProvisionProfile
+xcodebuild -workspace ${projectName}.xcworkspace -scheme ${projectName} -configuration Release clean build ARCHS='armv7 arm64' -sdk iphoneos10.2 CODE_SIGN_IDENTITY="iPhone Distribution: Leshi Co., Ltd" PROVISIONING_PROFILE=CommonEntpInHouseProvisionProfile
 
 xcrun -sdk iphoneos PackageApplication \
-`pwd`/DerivedData/HandyPushStream/build/Products/Release-iphoneos/HandyPushStream.app \
--o ~/Desktop/HandyPushStream${NE_BUILDID}.ipa
+`pwd`/DerivedData/${projectName}/build/Products/Release-iphoneos/${projectName}.app \
+-o ~/Desktop/${projectName}${NE_BUILDID}.ipa
 
 rm -rf build
