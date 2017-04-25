bab647ae20e6588430f5ab5d8e1edaed59ad3322
NearEast <teddy_nee@163.com>
2017-04-25 10:53:30 +0800

    完善脚本及忽略文件

--- createIPA.sh
+++ createIPA.sh
@@ -1,6 +1,13 @@
+#created by NearEast
+#updated 2017-05-04
+
+#默认使用当前文件夹的名字作为xCode工程的名字，并且当前文件夹为xCode工程所在的文件夹
+#如果不符合以上条件，修改脚本对应内容即可
 
 version=$1
 projectName=`pwd | awk -F "/" '{print $NF}'`
+outputPath=~/Desktop/tmp
+buildPath=${outputPath}/xcodeBuild
 
 echo 'using version number '${version}
 echo 'using projectName '${projectName}
@@ -16,10 +23,14 @@ echo 'using new version:${version}'
 /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${version}" ${projectName}/Info.plist
 fi
 
-xcodebuild -workspace ${projectName}.xcworkspace -scheme ${projectName} -configuration Release clean build ARCHS='armv7 arm64' -sdk iphoneos10.2 CODE_SIGN_IDENTITY="iPhone Distribution: Leshi Co., Ltd" PROVISIONING_PROFILE=CommonEntpInHouseProvisionProfile
+xcodebuild -workspace ${projectName}.xcworkspace -scheme ${projectName} -configuration Release clean build ARCHS='armv7 arm64' -sdk iphoneos CODE_SIGN_IDENTITY="iPhone Distribution: Leshi Co., Ltd" PROVISIONING_PROFILE=CommonEntpInHouseProvisionProfile SYMROOT="${buildPath}"
 
 xcrun -sdk iphoneos PackageApplication \
-`pwd`/DerivedData/${projectName}/build/Products/Release-iphoneos/${projectName}.app \
--o ~/Desktop/${projectName}${NE_BUILDID}.ipa
+${buildPath}/Release-iphoneos/${projectName}.app \
+-o ${outputPath}/${projectName}${NE_BUILDID}.ipa
 
-rm -rf build
+echo 'output ipa: '${outputPath}'/'${projectName}${NE_BUILDID}'.ipa'
+
+rm -rf ${build_path}
+
+
