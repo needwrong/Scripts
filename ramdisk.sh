6f683ce4532f6820429ac427d5a2fb11933a0914
NearEast <teddy_nee@163.com>
2017-04-25 10:55:38 +0800

    添加创建虚拟内存的脚本

---
+++ ramdisk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#reference: http://www.cocoachina.com/ios/20170425/19116.html
+#记得添加执行权限：chmod +x ramdisk.sh
+#并且可能需要使用Sudo执行
+
+RAMDISK=ramdisk
+SIZE=1024         #size in MB for ramdisk.
+diskutil erasevolume HFS+ $RAMDISK \
+     `hdiutil attach -nomount ram://$[SIZE*2048]`
